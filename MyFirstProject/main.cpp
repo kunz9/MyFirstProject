526c3dbbaa854ed2036372403f6c4bc377887140
kunz9 <111894457+kunz9@users.noreply.github.com>
2022-08-26 10:24:31 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,16 @@
+/*
+	Dakota Kunz
+	C++ Fall 2022
+	Due: Aug. 31, 2022
+	Lab ! Introduction to C++ Programming
+	This Lab is a demo of my ability to work with an IDE and Github. 
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << "Red";
+
+	return 0;
+}
